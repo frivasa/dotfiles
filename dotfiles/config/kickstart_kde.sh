7477fd9e844ba23495a821be315cd098e3d42d66
Fdo <rivas.aguilera@gmail.com>
2025-06-13 12:39:49 -0400

    lean install test fixes.

--- dotfiles/config/kickstart_kde.sh
+++ dotfiles/config/kickstart_kde.sh
@@ -3,11 +3,12 @@ git clone https://aur.archlinux.org/yay.git ~/Downloads/yay
 cd ~/Downloads/yay
 makepkg -si
 
-sudo pacman -S keepassxc wl-clipboard docker filelight vlc zsh obsidian zsh-syntax-highlighting zsh-autosuggestions ttf-jetbrains-mono-nerd tmux imagemagick gwenview handbrake syncthing obs-studio nodejs npm unzip rclone video-downloader steam qbittorrent fastfetch python-pywal yazi okular ollama
-yay -S dotdrop zen-browser-bin vesktop-bin konsave kwin-scripts-krohnkite-git plasma6-applets-panel-colorizer kwin-effect-rounded-corners-git
+sudo pacman -S keepassxc wl-clipboard docker filelight vlc zsh obsidian zsh-syntax-highlighting zsh-autosuggestions ttf-jetbrains-mono-nerd tmux imagemagick gwenview handbrake syncthing obs-studio nodejs npm tree-sitter-cli unzip rclone steam qbittorrent fastfetch yazi okular ollama python-pywal bc python-colorthief
+yay -S dotdrop zen-browser-bin vesktop-bin konsave kwin-scripts-krohnkite-git plasma6-applets-panel-colorizer kwin-effect-rounded-corners-git video-downloader
 
 export ZSH=~/.config/ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 curl -fsSL https://pixi.sh/install.sh | sh
 
 dotdrop install -p kde_desktop -f --cfg=~/dotfiles/dotdrop.config
